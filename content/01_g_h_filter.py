92a51b81102921a3ab75aa00bfc80a51ac17318c
Adrien Babet <babetdev@gmail.com>
2026-06-27 01:24:26 -0400

    added file

---
+++ content/01_g_h_filter.py
@@ -0,0 +1,28 @@
+import marimo
+
+__generated_with = "0.23.9"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    slider = mo.ui.slider(0,10)
+    slider
+    return (slider,)
+
+
+@app.cell
+def _(slider):
+    slider.value
+    return
+
+
+if __name__ == "__main__":
+    app.run()
